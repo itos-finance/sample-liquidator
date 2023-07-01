6199447a42ed6f3dbdb884779b1d544f8ab4a0d5
itoscj <cj@itos.fi>
2023-07-01 14:29:24 -0400

    cleanup, add more instructions, still untested: diamond address calc is tricky with my env setup since versions need to be in sync for init code

--- Liquidate.py
+++ Liquidate.py
@@ -1,14 +1,13 @@
 from ChainListener import get_provider
 from web3 import Web3
 from lib.Utils import derive_portfolio_id
-import struct
-from lib.Errors import TokenNotInRegistry
+from lib.Errors import TokenNotInResolverRegistry
 import lib.InstructionsLib as InstructionsLib
 
 class Liquidator:
     #X128 val used in calculation
     BASEX128 = 1 << 128
-    # when token isn't in the registry, it returns the max uint16 value as an error code
+    # when token isn't in the registry, the resolver returns the max uint16 value as an error code
     NOT_IN_REGISTRY_CODE = 65535
 
     def __init__(
@@ -30,15 +29,15 @@ class Liquidator:
         self.pocketbook_contract = provider.eth.contract(address = pm_contract_address, abi = pocketbook_contract_abi)
         self.resolver_contract = provider.eth.contract(address = resolver_address, abi = resolver_contract_abi)
 
-        # get the util values from getter facet and set them:
+        # get the static pm config values from getter facet and set them:
         self.maxUtil = self.pm_getter_contract.functions.maxUtil().call()
         self.liqToken = self.pm_getter_contract.functions.fallbackToken().call()
         self.targetUtil = self.pm_getter_contract.functions.targetUtil().call()
         self.liquidationBonus = self.pm_getter_contract.functions.liquidationBonus().call()
 
-    #to be called by a function that calls the api to get accounts and discovers one with unhealthy positions
+    #to be called by a function that calls the api to get accounts and discovers one with a liquidate-able portfolio
     def liquidate_account(self, account, safeToken):
-        print("liq")
+        print("Looking for liquidate-able portfolio on account...")
         portfolios = self.pm_getter_contract.functions.getAllPortfolios(account).call()
         print(portfolios)
 
@@ -48,7 +47,14 @@ class Liquidator:
             portfolio_id = derive_portfolio_id(account, i)
 
             #get status of position
-            (portfolio_collateral, portfolio_debt, portfolio_req, portfolio_util, portfolio_tails, portfolio_tailDebts, portfolio_tailCredits, portfolio_utils) = self.pm_contract.functions.queryValuesUSD(portfolio_id).call()
+            (portfolio_collateral,
+             portfolio_debt,
+             portfolio_req,
+             portfolio_util,
+             portfolio_tails,
+             portfolio_tailDebts,
+             portfolio_tailCredits,
+             portfolio_utils) = self.pm_contract.functions.queryValuesUSD(portfolio_id).call()
             print("     debt: ", portfolio_debt)
             print("     req: ", portfolio_req)
             print("     collateral: ", portfolio_collateral)
@@ -60,24 +66,23 @@ class Liquidator:
 
             # is it eligible for liquidation?
             if(((portfolio_util >= self.maxUtil and portfolio_util != 0)) or (portfolio_collateral > 0 and (portfolio_debt / portfolio_collateral >= self.maxUtil))):
-                print("     found portfolio to liquidate: ", portfolio_id)
+                print("     Found portfolio to liquidate: ", portfolio_id)
                 # get positions in the portfolio
                 positions = self.pm_getter_contract.functions.getPortfolio(account, i).call()
                 print("     Positions in portfolio:")
                 print("     ", positions)
                 # how do we liquidate?
-                instructions = self.getInstructions(portfolio_id, portfolio_collateral, portfolio_debt, self.liqToken, positions)
+                close_instructions = self.getInstructions(portfolio_id, portfolio_collateral, portfolio_debt, self.liqToken, positions)
 
                 pos_to_liq = []
                 for x in range(0, len(positions)):
                     pos_to_liq.append(Web3.to_int(positions[x]))
                 # call liquidate
-                self.pm_contract.functions.liquidate(portfolio_id, self.resolver_address, positions, [Web3.to_bytes(0x0),instructions,Web3.to_bytes(0x0),Web3.to_bytes(0x0)]).call()
+                self.pm_contract.functions.liquidate(portfolio_id, self.resolver_address, positions, [Web3.to_bytes(0x0),close_instructions,Web3.to_bytes(0x0),Web3.to_bytes(0x0)]).call()
 
         return "Healthy"
 
     def getInstructions(self, portfolio_id, portfolio_collateral, portfolio_debt, tokenOut, positions):
-        instructions = []
         valueToLiquidate = self.calcCreditAndDebtTargets(portfolio_debt, portfolio_collateral)
         print("valToLiq: " , valueToLiquidate)
         # get the tokens and amounts in the portfolio:
@@ -92,12 +97,15 @@ class Liquidator:
             if (valueToLiquidate[1] == portfolio_collateral):
                 # liquidate the whole portfolio
                 for tokenIndex in range(0, len(tokens)):
+                    # We need the token ids for the resolver:
                     token_in_id = self.get_token_id_from_address(tokens[tokenIndex])
                     token_out_id = self.get_token_id_from_address(tokenOut)
                     if token_in_id is None or token_out_id is None:
-                        raise TokenNotInRegistry()
+                        raise TokenNotInResolverRegistry()
+                    # TODO here we hard-code the tickSpacing to 50. Should make some call to get the tick spacing of given token pair's pool
                     return InstructionsLib.create_itos_swap_instruction(False, token_in_id, token_out_id, 0, credits[tokenIndex], 50)
-
+            # TODO: add liquidation if only part of the portfolio needs to be liquidated
+        # Temp default return value: a couple NOOPS
         return [Web3.to_bytes(0x0),Web3.to_bytes(0x0),Web3.to_bytes(0x0),Web3.to_bytes(0x0)]
 
     # Calculate how much needs to be liquidated. This calculation is based off of LiquidateLib:liquidationFormula() in the position manager
@@ -115,9 +123,9 @@ class Liquidator:
             credit_target = debt_target + ((debt_target * self.liquidationBonus) / self.BASEX128)
         return (debt_target, credit_target)
 
-    # get a the token id given the address of the token and an index to start searching at
+    # get a the token id from the resolver given the address of the token
     def get_token_id_from_address(self, token_address):
-        id = self.resolver_contract.functions.get_token_id_from_address(token_address).call()
+        id = self.resolver_contract.functions.getTokenIdFromAddress(token_address).call()
         if (id == self.NOT_IN_REGISTRY_CODE):
             return None
         else:
--- lib/InstructionsLib.py
+++ lib/InstructionsLib.py
@@ -4,16 +4,56 @@ import struct
 
 
 def create_itos_swap_instruction(is_exact_out, tokenIn, tokenOut, amountOut, amountIn, tickSpacing):
+    opcode = None
     if(is_exact_out == False):
+        # itos swap exact in
         opcode = Web3.to_bytes(3)
-        # token is uint16. Give 2 bytes:
-        tokenIn_bytes = struct.pack('>H', tokenIn)#Web3.to_bytes(int(str(tokenIn), 2))
-        print(len(tokenIn_bytes))
-        tokenOut_bytes = struct.pack('>H', tokenOut)
-        amountOut_bytes = Web3.to_bytes(int(str(amountOut), 32))
-        amountIn_bytes = Web3.to_bytes(int(str(amountIn), 16))
-        tickSpacing_bytes = struct.pack('>L', tickSpacing)[:3]
-        format_str= 'ssssss'
-        instruction = struct.pack(format_str,opcode, amountOut_bytes, amountIn_bytes, tickSpacing_bytes, tokenIn_bytes, tokenOut_bytes)
-        print(Web3.to_bytes(instruction))
-        return Web3.to_bytes(instruction)
+    else:
+        # itos swap exact in
+        opcode = Web3.to_bytes(1)
+
+    # token is uint16. Give 2 bytes:
+    tokenIn_bytes = struct.pack('>H', tokenIn) #Web3.to_bytes(int(str(tokenIn), 2))
+    print(len(tokenIn_bytes))
+    tokenOut_bytes = struct.pack('>H', tokenOut)
+    # amountOut is uint256
+    amountOut_bytes = Web3.to_bytes(int(str(amountOut), 32))
+    # amountIn is uint128
+    amountIn_bytes = Web3.to_bytes(int(str(amountIn), 16))
+    # uint24
+    tickSpacing_bytes = struct.pack('>L', tickSpacing)[:3]
+    # see https://docs.python.org/3/library/struct.html#format-characters
+    format_str= 'ssssss'
+    instruction = struct.pack(format_str, opcode, amountOut_bytes, amountIn_bytes, tickSpacing_bytes, tokenIn_bytes, tokenOut_bytes)
+    print(Web3.to_bytes(instruction))
+    return Web3.to_bytes(instruction)
+
+def create_uniswap_instruction(is_exact_out, tokenIn, tokenOut, amountOut, amountIn, fee):
+    opcode = None
+    if(is_exact_out == False):
+        # uniswap exact in
+        opcode = Web3.to_bytes(4)
+    else:
+        # uniswap exact out
+        opcode = Web3.to_bytes(2)
+
+    # token is uint16. Give 2 bytes:
+    tokenIn_bytes = struct.pack('>H', tokenIn) #Web3.to_bytes(int(str(tokenIn), 2))
+    print(len(tokenIn_bytes))
+    tokenOut_bytes = struct.pack('>H', tokenOut)
+    amountOut_bytes = Web3.to_bytes(int(str(amountOut), 32))
+    amountIn_bytes = Web3.to_bytes(int(str(amountIn), 16))
+    fee_bytes = struct.pack('>L', fee)[:3]
+    format_str= 'ssssss'
+    instruction = struct.pack(format_str, opcode, amountOut_bytes, amountIn_bytes, fee_bytes, tokenIn_bytes, tokenOut_bytes)
+    print(Web3.to_bytes(instruction))
+    return Web3.to_bytes(instruction)
+
+def create_transferFrom_instruction(amount, token):
+    opcode = Web3.to_bytes(5)
+    amount_bytes = Web3.to_bytes(int(str(amount), 32))
+    token_bytes = struct.pack('>H', token)
+    format_str= 'sss'
+    instruction = struct.pack(format_str, opcode, amount_bytes, token_bytes)
+    print(Web3.to_bytes(instruction))
+    return Web3.to_bytes(instruction)
