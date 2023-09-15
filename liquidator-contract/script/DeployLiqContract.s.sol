412e47beed199e7c19733c102c8c301ff8c5ed7f
itoscj <122551116+itoscj@users.noreply.github.com>
2023-09-15 17:32:04 -0400

    update contracts (#6)

--- liquidator-contract/script/DeployLiqContract.s.sol
+++ liquidator-contract/script/DeployLiqContract.s.sol
@@ -11,12 +11,12 @@ contract DeployLiqContractScript is Script {
     function run() public {
         address deployerAddr = vm.envAddress("DEPLOYER_PUBLIC_KEY");
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        address balancerAddr =  0x7a3bdA2705145eb4c45ff6C2f079FbCB70E22e84;
-        address positionManagerAddr = vm.envAddress("diamond");
-        address usdc = vm.envAddress("usdc");
-        address weth = vm.envAddress("weth");
-        address router = vm.envAddress("router");
-        address factory = vm.envAddress("factory");
+        address balancerAddr =  0x2B0d36FACD61B71CC05ab8F3D2355ec3631C0dd5;
+        address positionManagerAddr = vm.envAddress("DIAMOND");
+        address usdc = vm.envAddress("USDC");
+        address weth = vm.envAddress("WETH");
+        address router = vm.envAddress("ROUTER");
+        address factory = vm.envAddress("FACTORY");
         vm.startBroadcast(deployerPrivateKey);
         Liquidator liq = new Liquidator(positionManagerAddr, balancerAddr, router, factory);
 
